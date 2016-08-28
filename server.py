0c1ff489d076a5fb69aa8487e592b7bed009574c
Ádám Horváth <horada1991@gmail.com>
2016-08-28 13:27:22 +0200

    init database

--- server.py
+++ server.py
@@ -14,8 +14,22 @@ def get_db():
 
 
 @app.teardown_appcontext
-def close_db():
+def close_db(exception):
     """Close the db at the end of the request"""
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
+
+def initdb():
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    print('Initialized the database.')
+
+
+# with app.app_context():
+#     print(initdb())
+#     print(current_app.name + ' started')
